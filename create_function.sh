3716430e06d0ec13eb39e947455c6e63047cbd66
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-03-13 13:50:03 +0200

    Add files

--- create_function.sh
+++
@@ -1,6 +0,0 @@
-az login
-az account set --subscription IC3-PSTNDYNRTG-DEV
-az group create --name dev-andres-test --location northeurope
-az storage account create --name andresdevstorage --location northeurope --resource-group dev-andres-test --sku Standard_LRS
-az functionapp create --resource-group dev-andres-test --consumption-plan-location northeurope --runtime dotnet-isolated --functions-version 4 --name dev-andres-test --storage-account andresdevstorage
-func azure functionapp publish dev-andres-test
